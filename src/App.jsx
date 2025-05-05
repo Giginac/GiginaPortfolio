ce18a180d344b5a35955b08c42b287db3cf3e013
Gigina Rajesh <giginacleetus18@gmail.com>
2025-05-05 18:47:49 +0100

    updtaed contacts

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,21 @@ import Skills from './components/Skills'
 import Projects from './components/projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer' 
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
+ 
   useEffect(()=>{
     AOS.init({
       duration: 1000, 
     });
+   
   },[]);
   return (
+
     <>
     <ParticleBackground />
+    <ToastContainer/>
     <Navbar />
     <Hero />
     <Skills />
@@ -25,6 +30,7 @@ const App = () => {
     <Contact />
     <Footer />
     </>
+     
   )
 }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import { FiMail, FiUser, FiMessageSquare } from 'react-icons/fi';
 import { motion } from 'framer-motion';
+import { toast } from 'react-toastify';
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+  
+      const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+    
+        // Enter your Web3Forms Access Key below
+        formData.append("access_key", "416af221-9419-48fa-baab-7344a67822ed");
+    
+        const response = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          body: formData
+        });
+    
+        const data = await response.json();
+    
+        if (data.success) {
+          setResult("");
+          toast.success("Message Sent",{
+            position: 'top-center',
+            theme: 'dark',
+          });
+          event.target.reset();
+        } else {
+          console.log("Error", data);
+          toast.error(data.message)
+          setResult("");
+        }
+      };
+
   return (
     <section data-aos="fade-up" data-aos-delay="250" id="contact" className="py-20 px-4 bg-gradient-to-b">
       <div className="max-w-lg mx-auto relative">
@@ -14,7 +47,7 @@ const Contact = () => {
         </div>
         {/* Contact Form */}
        
-          <form className="space-y-6 bg-gray-200/85 p-8 rounded-xl shadow-md border border-gray-100">
+          <form onSubmit={onSubmit} className="space-y-6 bg-gray-200/85 p-8 rounded-xl shadow-md border border-gray-100">
             {/* Name Field */}
             <motion.div whileHover={{ scale: 1.01 }}>
               <label className="block text-gray-700 mb-2">Name</label>
@@ -23,10 +56,10 @@ const Contact = () => {
                   <FiUser className="text-gray-400" />
                 </div>
                 <input
-                  type="text"
+                  type="text" name="Name"
                   className="w-full pl-10 pr-4 py-3 border border-gray-200 rounded-lg focus:ring-2
                    focus:ring-red-400 focus:border-transparent"
-                  placeholder="Enter your name"
+                  placeholder="Enter your name" required
                 />
               </div>
             </motion.div>
@@ -39,10 +72,10 @@ const Contact = () => {
                   <FiMail className="text-gray-400" />
                 </div>
                 <input
-                  type="email"
+                  type="email" name="Email"
                   className="w-full pl-10 pr-4 py-3 border border-gray-200 rounded-lg focus:ring-2
                    focus:ring-red-400 focus:border-transparent"
-                  placeholder="Enter your email"
+                  placeholder="Enter your email" required
                 />
               </div>
             </motion.div>
@@ -54,11 +87,11 @@ const Contact = () => {
                 <div className="absolute top-3 left-3">
                   <FiMessageSquare className="text-gray-400" />
                 </div>
-                <textarea
+                <textarea name="Message"
                   className="w-full pl-10 pr-4 py-3 border border-gray-200 rounded-lg focus:ring-2
                    focus:ring-red-400 focus:border-transparent"
                   rows="4"
-                  placeholder="Write your message"
+                  placeholder="Write your message" required
                 ></textarea>
               </div>
             </motion.div>
@@ -71,14 +104,14 @@ const Contact = () => {
               className="w-full py-3 px-6 bg-yellow-500 text-white rounded-lg font-medium
                hover:bg-yellow-400 transition-all"
             >
-              Send Message
+               {result ? result : "Send Message"}
             </motion.button>
           </form>
         </div>
 
         {/* Alternative Contact */}
         <div className="mt-8 text-center text-white">
-          <p>Or email me directly at: <span className="text-white-950 font-bold">Giginacleetus@gmail.com</span></p>
+          <p>Or email me directly at: <span className="text-white-950 font-bold">Giginacleetus18@gmail.com</span></p>
         </div>
     </section>
   );
